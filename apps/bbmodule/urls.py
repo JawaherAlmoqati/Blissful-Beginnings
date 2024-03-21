046d124831e7a7c870c352d8a781ef11cbca4796
JawaherAlmoqati <jawaher.almshhab@gmail.com>
2024-03-21 18:32:42 +0300

    first commit

--- apps/bbmodule/urls.py
+++ apps/bbmodule/urls.py
@@ -4,5 +4,7 @@ from apps.bbmodule import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('bb',views.bb),
-    path('bb1/<int:bID>', views.bb1)
+    path('bb1/<int:bID>', views.bb1),
+    path('bbmodule', views.bb)
+
 ]
